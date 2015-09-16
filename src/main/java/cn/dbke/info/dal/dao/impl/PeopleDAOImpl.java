78884de9fb97d603186c0199e8291be4dd06339d
liyangtao <yangtao.lyt@alibaba-inc.com>
2015-09-16 18:19:50 +0800

    提交代码

--- src/main/java/cn/dbke/info/dal/dao/impl/PeopleDAOImpl.java
+++ src/main/java/cn/dbke/info/dal/dao/impl/PeopleDAOImpl.java
@@ -2,8 +2,7 @@ package cn.dbke.info.dal.dao.impl;
 
 import cn.dbke.info.dal.dao.PeopleDAO;
 import cn.dbke.info.dal.dbobject.PeopleDO;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -20,17 +19,10 @@ public class PeopleDAOImpl implements PeopleDAO,InitializingBean{
     private static String NAME_SPACE = "PeopleDAO.";
 
     @Resource
-    private SqlSessionFactory sqlSessionFactory;
+    private SqlSessionTemplate sqlSessionTemplate;
 
     public void insert(PeopleDO people) {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        try{
-            sqlSession.insert("cn.dbke.info.dal.dao.PeopleDAO.insertPeople",people);
-            sqlSession.commit();
-        }finally{
-            sqlSession.close();
-        }
-
+        sqlSessionTemplate.insert("cn.dbke.info.dal.dao.PeopleDAO.insertPeople",people);
     }
 
     public void afterPropertiesSet() throws Exception {
